075d077825503ea7ad1326d9ec39d4372bd73097
Sujan Shrestha <Sujan.shrestha@agile.com.np>
2024-07-03 16:31:14 +0545

    first commit

---
+++ Core/Entities/Product.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Core.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
