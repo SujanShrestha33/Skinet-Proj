09e95962323cd2a6206be1625351c7a2a8c70366
Sujan Shrestha <Sujan.shrestha@agile.com.np>
2024-07-09 15:08:31 +0545

    section 6

--- API/Extensions/ApplicationServiceExtension.cs
+++ API/Extensions/ApplicationServiceExtension.cs
@@ -30,6 +30,13 @@ namespace API.Extensions
                     return new BadRequestObjectResult(errorResponse);
                 };
             });
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
 
             return services;
         }
